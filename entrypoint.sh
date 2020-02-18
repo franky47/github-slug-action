0e42c48f8cc1462b52f67acb62d36f0634c03e51
Francois Best <francois@francoisbest.com>
2020-02-18 17:05:46 +0100

    test: Add malicious env var
    This is a test to check 47ng/actions-clever-cloud against
    malicious environment variables set in rogue actions
    running before it.
    
    See 47ng/actions-clever-cloud#8.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,3 @@
 #!/bin/sh -l
 
-slug_ref() {
-    echo "$1" \
-         | tr "[:upper:]" "[:lower:]" \
-         | sed -r 's#refs/[^\/]*/##;s/[~\^]+//g;s/[^a-zA-Z0-9]+/-/g;s/^-+\|-+$//g;s/^-*//;s/-*$//' \
-         | cut -c1-63
-}
-
-short_sha(){
-  echo "$1" \
-        | cut -c1-8
-}
-
-echo ::set-env name=GITHUB_REF_SLUG::"$(slug_ref "$GITHUB_REF")"
-echo ::set-env name=GITHUB_HEAD_REF_SLUG::"$(slug_ref "$GITHUB_HEAD_REF")"
-echo ::set-env name=GITHUB_BASE_REF_SLUG::"$(slug_ref "$GITHUB_BASE_REF")"
-echo ::set-env name=GITHUB_SHA_SHORT::"$(short_sha "$GITHUB_SHA")"
+echo ::set-env name=INPUT_CLEVER_ENV_MALICIOUS_ENV_TEST::"https://github.com/47ng/actions-clever-cloud/issues/8"
